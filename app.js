07aff6f13af9438256bdbd90a21275a5a8f22e6d
Darshana Perera <darshana.saluka.10@gmail.com>
2022-07-11 10:38:49 +0530

    get request modified'

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get("/",(req,res)=>{
-    res.send("first request!");
+    let users = ["darshana","saluka","perera"]
+    res.send(users);
 })
 
 app.listen(3000,()=>{
