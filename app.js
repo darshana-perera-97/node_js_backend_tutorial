f8ae7e3bfa1e57f4f45890b0f7c19257630504a7
Darshana Perera <darshana.saluka.10@gmail.com>
2022-07-11 11:02:22 +0530

    middleware added to app.js

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const express = require('express');
 
 const app = express();
 
+const customMiddleware = ()=>{
+    console.log("Welcome to my middleware");
+}
+
+app.use(customMiddleware);
+
 app.get("/",(req,res)=>{
+    res.send("Server started");
+})
+app.get("/users",(req,res)=>{
     let users = ["darshana","saluka","perera"]
     res.send(users);
+})
+app.get("/create_users",(req,res)=>{
+    res.send(req);
 })
 
 app.listen(3000,()=>{
